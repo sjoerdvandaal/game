9ac23e95e8c2c07f12514fc882816e3d801c2e79
Sjoerd van Daal <sjoerd.van.daal@proton.me>
2025-10-13 10:26:15 +0200

    background music

--- game/App.xaml.cs
+++ game/App.xaml.cs
@@ -1,14 +1,28 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
 using System.Windows;
+using System.Windows.Media;
 
 namespace game
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public static MediaPlayer BackgroundMusic;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            BackgroundMusic = new MediaPlayer();
+            BackgroundMusic.Open(new Uri("Audio/AudioGame.mp3", UriKind.Relative));
+            BackgroundMusic.Volume = 1.0;
+
+            BackgroundMusic.MediaEnded += (s, args) =>
+            {
+                BackgroundMusic.Position = TimeSpan.Zero;
+                BackgroundMusic.Play();
+            };
+
+            BackgroundMusic.Play(); // start music
+        }
+    }
 }
